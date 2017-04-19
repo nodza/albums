8bc76e30ca8ea9ad2793ae79d442d85b40676c24
nodza <noel.hwande@gmail.com>
2017-04-19 16:09:26 -0500

    First commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>List of albums...</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
